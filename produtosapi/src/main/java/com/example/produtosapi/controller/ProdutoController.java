4598c46520f76a5016388e0bad485415b38cf6f3
Letícia Fernandes <lemayara16@gmail.com>
2024-11-08 13:10:39 -0300

    obtenção de dados do produto

--- produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
@@ -2,10 +2,7 @@ package com.example.produtosapi.controller;
 
 import com.example.produtosapi.model.Produto;
 import com.example.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -29,5 +26,10 @@ public class ProdutoController {
         produtoRepository.save(produto);
 
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
     }
 }
