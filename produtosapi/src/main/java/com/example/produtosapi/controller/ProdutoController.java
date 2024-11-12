4ab0c245fff7ab029d8ee354208602f917b98b13
Letícia Fernandes <lemayara16@gmail.com>
2024-11-12 12:31:00 -0300

    realização de busca por nome do produto

--- produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.example.produtosapi.model.Produto;
 import com.example.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -42,5 +43,10 @@ public class ProdutoController {
     public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
         produto.setId(id);
         produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNome(nome);
     }
 }
